94bd29f5c6f3fdb215bf0320d781fcd84887f983
Roberto Alcantara <roberto@eletronica.org>
2019-06-29 21:26:32 -0300

    Superficie adicionado

--- piezo/surface.py
+++ piezo/surface.py
@@ -17,21 +17,140 @@ def closest(sorted_dict, key):
     return min(keys, key=lambda k: abs(key - k))
 
 
-def neigh( dic, center):
-    found = False;
-    prev = None
-    next = None
+def curve_process( data ):
 
-    for i in dic:
-        if found:
-		next  = i
-		return prev,next
-	if i==center:
-		#found
-		found = True	
-	else:			
-		prev = i;
-    return None, None	
+        print len(data)
+
+        vl = []
+        il = []
+        pl = []
+        for i in data:
+            if i[1] < 0.01:
+                continue
+            il.append( i[1] )
+            vl.append( i[0] )
+            pl.append( i[1] * i[0] / 1000 )
+            # print (str(i[1]) + "," + str(i[0]))  #CSV OUTPUT Curve
+        if len( il ) < 5:
+            print "Nao possui amostras suficiente"
+            return False 
+
+        print len(vl)
+
+        coef = np.polyfit(il,vl,3) #6
+
+        vl_calc = np.polyval(coef,il)
+        pl_calc = np.array(il) * np.array(vl_calc) /1000
+
+        #calcular o erro
+        print "--------------------"
+        err_v = np.array(vl) - np.array(vl_calc)
+        #nao necessario err_v[0] = 0 #amostra 0 eh especial pq representa a saida a vazio
+        print "V Erro medio: " + str ( err_v.mean() ) + "mV"
+        print "V Desvio padrao erro: " + str ( err_v.std() ) + "mV"
+        print "-------------------"
+
+        ### curva calculada:
+        MAX_VOUT = 5004.5 #mV  DAC MAX
+        MIN_VOUT = 0
+        VDAC_SIZE = 4096
+        VLSB_SIZE = MAX_VOUT / VDAC_SIZE
+
+        RSENSE = 1.045 #OHM
+
+        MAX_IOUT = 20 #mA
+        MIN_IOUT = 0
+        #ILSB_SIZE = 0.00575337029108412  #mA
+        ILSB_SIZE = 0.00000092832880672904 *1000    #em mA
+
+        i_calc = np.arange(0, 4094, 1)
+        i_calc = np.array(i_calc) * ILSB_SIZE
+        v_calc = np.polyval(coef, i_calc )
+        aux = []
+        cnt = 0
+        for x in v_calc: #cleanup
+            if x<0:
+                aux.append(0)
+            else: #adiciona offset do shunt - dobrado porque ele existe na captura e vai existir na reproducao
+                v_ad = x + ( RSENSE * i_calc[cnt] * 2)
+                v_ad = v_ad + 200 # offset ajuste
+                aux.append(v_ad)
+            cnt = cnt + 1
+
+        v_calc = aux
+        p_calc = np.array(i_calc) * np.array(v_calc) /1000
+        #calcula o codigo:
+        i_ad = []
+        for i in i_calc:
+            i_ad.append( int(i / ILSB_SIZE) )
+        v_ad = []
+
+        for v in v_calc:
+            if v > 2000:
+                return False #erro de condicionamento
+            v_ad.append( int(v / VLSB_SIZE) )
+
+
+        #Ajuste da escala de tempo
+        time = []
+        tcnt = 0
+        for cnt in range (0, len(data)):
+            time.append(tcnt)
+            tcnt = tcnt + 0.00001976 #50.61 kHz
+        print "Numero itens na curva: " + str( len(data) )
+
+        show_graph( vl, il, pl, vl_calc, pl_calc, v_calc, i_calc, p_calc, v_ad, i_ad  )
+
+
+def show_graph(vl, il, pl, vl_calc,pl_calc, v_calc, i_calc, p_calc, v_ad, i_ad ):
+        #fig, ax1 = plt.subplots()
+        #ax1.grid(True)
+  
+        '''ax1.plot(il,vl, 'bx')
+
+        ax1.set_xlabel('Current (mA)')
+        ax1.tick_params('y', colors='b')
+        ax1.set_ylabel("Voltage (mV)")
+
+        #ax1.plot(il, vl_calc, 'bx')
+        ax2 = ax1.twinx()
+        ax2.tick_params('y', colors='r')
+
+        ax2.plot(il,pl_calc, 'r-')
+        ax2.set_ylabel('Power (mW)' )
+        ax2.grid(False)
+
+        #ax2.plot(il, pl_calc, 'r-')
+        fig.show()'''
+
+        '''
+        fig2,bx1 = plt.subplots()
+        bx1.grid(True)
+        bx1.plot(i_calc, v_calc, 'b-')
+        bx1.set_xlabel('Current (mA)')
+        bx1.tick_params('y', colors='b')
+        bx1.set_ylabel("Voltage (mV)")
+        bx2 = bx1.twinx()
+        bx2.tick_params('y', colors='r')
+        bx2.plot(i_calc,p_calc,'r-')
+        bx2.set_ylabel("Power (mW)")
+        bx2.grid(False)
+        fig2.show()
+        '''
+        
+        fig3,cx1 = plt.subplots()
+        cx1.grid(True)
+        cx1.plot(i_ad, v_ad, 'bx')
+        cx1.set_xlabel('Current Code')
+        cx1.tick_params('y', colors='b')
+        cx1.set_ylabel("Voltage Code'")
+        fig3.show()
+        
+        plt.show()
+
+
+##################################################
+# Main
 
 if len(sys.argv) < 2:
     print "filename missing"
@@ -153,8 +272,6 @@ while True:
 
 
 print "Numero amostras: " + str(len(data))
-data2 = SortedDict()
-
 
 curve_idx = -1
 aux = not data_trace1[0]
@@ -176,227 +293,12 @@ for t in data_trace1:
     cnt_byte_curve = cnt_byte_curve+1       
 
 
-#data2 = SortedDict()
 
-#conta_ocorrencia = SortedDict()
-#for x in curves[1005]:
-#    v = x[0]
-#    i = x[1]
-#    if data2.get(i)==None:
-#        data2[i] = v
-    #    conta_ocorrencia[i] = 1
-    #else:
-    #    data2[i] = (data2.get(i) + v) /2
-    #    conta_ocorrencia[i] = conta_ocorrencia[i] + 1;
-
-CURVA = 3660
-
-data = []
+#fiv = open("ivsurface.bin", 'wb')
 for k in curves:
-    for x in k:
-      v = x[0]
-      i = x[1]
-      data.append( [v,i] )
-
-'''conta_ocorrencia = SortedDict()
-for x in data:
-    v = x[0]
-    i = x[1]
-    if data2.get(i)==None:
-        data2[i] = v
-        conta_ocorrencia[i] = 1
-    else:
-        data2[i] = v
-        #data2[i] = (data2.get(i) + v) /2
-        conta_ocorrencia[i] = conta_ocorrencia[i] + 1;    
-'''
-#data = []
-#for i, v in data2.items():
-#    if conta_ocorrencia[i] > 1: # 2 filtro para ruidos da amostragem
-#        data.append( [v, i] )
-
-
-
-print len(data)
-
-
-vl = []
-il = []
-pl = []
-for i in data: # curves[600]:
-    if i[0] < 0: continue
-    il.append( i[1] )
-    vl.append( i[0] )
-    pl.append( i[1] * i[0] / 1000 )
-    # print (str(i[1]) + "," + str(i[0]))  #CSV OUTPUT Curve
-
-print len(vl)
-
-coef = np.polyfit(il,vl,4) #6
-
-vl_calc = np.polyval(coef,il)
-pl_calc = np.array(il) * np.array(vl_calc) /1000
-
-#calcular o erro
-print "--------------------"
-err_v = np.array(vl) - np.array(vl_calc)
-#nao necessario err_v[0] = 0 #amostra 0 eh especial pq representa a saida a vazio
-print "V Erro medio: " + str ( err_v.mean() ) + "mV"
-print "V Desvio padrao erro: " + str ( err_v.std() ) + "mV"
-print "-------------------"
-
-### curva calculada:
-MAX_VOUT = 5004.5 #mV  DAC MAX
-MIN_VOUT = 0
-VDAC_SIZE = 4096
-VLSB_SIZE = MAX_VOUT / VDAC_SIZE
-
-RSENSE = 1.045 #OHM
-
-MAX_IOUT = 20 #mA
-MIN_IOUT = 0
-#ILSB_SIZE = 0.00575337029108412  #mA
-ILSB_SIZE = 0.00000092832880672904 *1000    #em mA
-
-i_calc = np.arange(0, 4094, 1)
-i_calc = np.array(i_calc) * ILSB_SIZE
-v_calc = np.polyval(coef, i_calc )
-aux = []
-cnt = 0
-for x in v_calc: #cleanup
-    if x<0:
-        aux.append(0)
-    else: #adiciona offset do shunt - dobrado porque ele existe na captura e vai existir na reproducao
-        v_ad = x + ( RSENSE * i_calc[cnt] * 2)
-        v_ad = v_ad + 200 # offset ajuste
-        aux.append(v_ad)
-    cnt = cnt + 1
-
-
-v_calc = aux
-p_calc = np.array(i_calc) * np.array(v_calc) /1000
-#calcula o codigo
-i_ad = []
-for i in i_calc:
-    i_ad.append( int(i / ILSB_SIZE) )
-v_ad = []
-
-for v in v_calc:
-    v_ad.append( int(v / VLSB_SIZE) )
-
-
-
-#Ajuste da escala de tempo
-time = []
-tcnt = 0
-for cnt in range (0, len(data)):
-    time.append(tcnt)
-    tcnt = tcnt + 0.00001976 #50.61 kHz
-
-
-
-
-
-'''
-
-
-fig, ax1 = plt.subplots()
-ax1.grid(True)
-ax1.plot(il,vl, 'bx')
-ax1.set_xlabel('Current (mA)')
-ax1.tick_params('y', colors='b')
-ax1.set_ylabel("Voltage (mV)")
-ax1.plot(il, vl_calc, 'b-')
-ax2 = ax1.twinx()
-ax2.tick_params('y', colors='r')
-#ax2.plot(il,pl, 'rx')
-ax2.set_ylabel('Power (mW)' )
-ax2.grid(False)
-ax2.plot(il,pl_calc, 'r-')
-fig.show()
-
-
-fig2,bx1 = plt.subplots()
-bx1.grid(True)
-bx1.plot(i_calc, v_calc, 'b-')
-bx1.set_xlabel('Current (mA)')
-bx1.tick_params('y', colors='b')
-bx1.set_ylabel("Voltage (mV)")
-bx2 = bx1.twinx()
-bx2.tick_params('y', colors='r')
-bx2.plot(i_calc,p_calc,'r-')
-bx2.set_ylabel("Power (mW)")
-bx2.grid(False)
-fig2.show()
-
-
-fig3,cx1 = plt.subplots()
-cx1.grid(True)
-cx1.plot(i_ad, v_ad, 'bx')
-cx1.set_xlabel('Current Code')
-cx1.tick_params('y', colors='b')
-cx1.set_ylabel("Voltage Code'")
-fig3.show()
-
-
-plt.show()
-'''
-v = []
-i = []
-time = []
-tcnt = 0
-for x in data:
-    v.append(x[0])
-    i.append(x[1])
-
-    time.append(tcnt)
-    tcnt = tcnt + 0.00001976 #50.61 kHz
-    if len(v)>100000:
-        break
-
-
-print len(time)
-print len(v)
-print len(i)
-
-#i = np.array(i)
-#v = np.array(v)
- 
-## Plot the surface.
-#fig = plt.figure()
-#ax=fig.gca(projection='3d')
-# Make data.
-#X = np.arange(0, time[ len(time)-1 ], 0.1)
-#Y = np.arange(0, 2000, 0.1)
-#X, Y = np.meshgrid(X, Y, sparse=True)
-#R = np.sqrt(X**2 + Y**2)
-#Z = np.sin(R)
-#Z = i
-
-#surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,  linewidth=1, antialiased=False)
-
-#fig = plt.figure()
-#ax = fig.add_subplot(111, projection='3d')
-#ax.scatter(time,i,v, marker="x")
-
-#plt.xlim(0,3)
-#plt.ylim(0,0.3)
-
-# Customize the z axis.
-#ax.set_zlim(-1.01, 1.01)
-#ax.zaxis.set_major_locator(LinearLocator(10))
-#ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-
-# Add a color bar which maps values to colors.
-#fig.colorbar(surf, shrink=0.5, aspect=5)
-
-#plt.show()
-
-
-
-
-
-
+    ret = curve_process( k )
+    #if ret == False:
+    #    continue #verificar o que fazer aqui
 
 
 
